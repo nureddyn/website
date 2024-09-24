f0d5e18fcdb6c29e43b95c2d48f80ec9fe7e1208
Nureddyn Ballout <nureddynballout@gmail.com>
2024-09-24 19:31:46 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: "https://nureddyn.dev",
+  site: "https://www.nureddyn.dev",
   integrations: [
     mdx(),
     
