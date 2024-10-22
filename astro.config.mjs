1548ae75d313f18c4ad5288da0f798ffd4ee5bf4
Nureddyn <nureddynballout@gmail.com>
2024-10-22 18:45:19 -0400

    update dependencies and logo fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,9 @@ import mdx from "@astrojs/mdx";
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
+import icon from 'astro-icon';
+
 export default defineConfig({
   site: "https://nureddyn.dev",
-  integrations: [
-    mdx(),
-    
-    tailwind(),
-
-    react(),
-  ]
+  integrations: [mdx(), tailwind(), react(), icon()]
 });
